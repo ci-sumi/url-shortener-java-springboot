47bfacffb2d4a0d007d477ba0ce1b14e4cbb94fb
ci-sumi <tharayilsurendransumi@gmail.com>
2026-01-14 22:07:53 +0000

    Add thymeleaf dependencies and pass the message through

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -8,12 +9,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String home(){
-        return "index.html";
+    public String home(Model model){
+        model.addAttribute("message","R u there");
+        return "index";
     }
     @GetMapping("/a")
-    public String about(){
-        return "about.html";
+    public String about(Model model){
+        model.addAttribute("message","Hello Sumi How are you");
+        return "about";
     }
 
 }
